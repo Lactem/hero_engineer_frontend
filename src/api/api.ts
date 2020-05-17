b4999e44ba0114a853a7e3844824fb1980caf206
Lactem <tilie@usc.edu>
2020-05-17 14:34:41 -0700

    Change API to use HTTPS

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 
 // Don't forget to also change the API variable in public/avatars/js/svgavatars.core.min.js
-const apiBase = "http://www.heroengineer.com:8081/api";
+const apiBase = "https://www.heroengineer.com:8082/api";
 // const apiBase = "http://localhost:8081/api";
 
 export default apiBase;
